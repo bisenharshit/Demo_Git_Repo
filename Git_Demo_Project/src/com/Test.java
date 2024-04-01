409c7779ed17c93b6f087add4d886603ad41385d
harshit.bisen <harshit.bisen574@gmail.com>
2024-04-01 16:56:13 +0530

    By Harshit Bisen

--- Git_Demo_Project/src/com/Test.java
+++ Git_Demo_Project/src/com/Test.java
@@ -13,5 +13,12 @@ public class Test {
 		}else{
 			System.out.println("Number is Odd : "+num);
 		}
+		
+		int num1 = 15;
+		if(num%2==0){
+			System.out.println("Number is Even :"+num1);
+		}else{
+			System.out.println("Number is Odd : "+num1);
+		}
 	}
 }
