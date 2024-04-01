9f8686b5cc29843bd38340f30f7830845a764a68
bisenharshit <114408680+bisenharshit@users.noreply.github.com>
2024-04-01 16:54:12 +0530

    Update Test.java

--- Git_Demo_Project/src/com/Test.java
+++ Git_Demo_Project/src/com/Test.java
@@ -5,5 +5,13 @@ public class Test {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello Git");
+		System.out.println("Hello Git");
+
+		int num = 12;
+		if(num%2==0){
+			System.out.println("Number is Even :"+num);
+		}else{
+			System.out.println("Number is Odd : "+num);
+		}
 	}
 }
